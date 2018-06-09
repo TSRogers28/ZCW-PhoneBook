16499ac810e537b52cceff562a0b5179a68bf356
Tommy Rogers <tommyr@TommyRogers.local>
2018-06-09 09:57:57 -0400

    finished phonebook

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,7 +1,69 @@
- 
+import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.List;
+import java.util.Arrays;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private String name;
+    private String number;
+    Map<String,List<String>> phoneBook =new TreeMap<String, List<String>>();
+    List<String> numberSet = new ArrayList<String>();
+
+    public PhoneBook(){
+
+    }
+    public void addToPhoneBook(String name, String number){
+        ArrayList <String> temp = new ArrayList<>();
+        temp.add(number);
+        phoneBook.put(name, temp);
+
+    }
+    
+    public void addToPhoneBookIndividualNumbers(String name, String number){
+        numberSet.add(number);
+        phoneBook.put(name, numberSet);
+
+    }
+
+    public void removeFromPhoneBook(String name){
+
+        phoneBook.remove(name);
+
+    }
+
+    public void removeFromPhoneBookIndividualNumbers(String name, String number){
+        phoneBook.get(name).remove(number);
+
+    }
+
+    public String lookupFromPhoneBook(String name){
+        numberSet = phoneBook.get(name);
+        return numberSet.toString();
+    }
+
+    public String reverseLookupFromPhoneBook(String number){
+        String answerKey = null;
+        for(String key: phoneBook.keySet()){
+            List<String> test = phoneBook.get(key);
+            for(String s: test){
+                if(number.equals(s)){
+                    return key;
+                }
+
+            }
+        }
+        return "nothing is here";
+    }
+
+    public String displayFromPhoneBook(){
+
+        return phoneBook.entrySet().toString();
+
+    }
+
 }
--- PhoneBookTest.java
+++ PhoneBookTest.java
@@ -1,4 +1,4 @@
-
+import java.util.TreeMap;
 
 import static org.junit.Assert.*;
 import org.junit.After;
@@ -13,30 +13,98 @@ import org.junit.Test;
  */
 public class PhoneBookTest
 {
-    /**
-     * Default constructor for test class PhoneBookTest
-     */
-    public PhoneBookTest()
-    {
-    }
+    
+    PhoneBook phoneBook =new PhoneBook();
+    private String name;
+    private String number;
+    @Test
+    public void testAddToPhoneBook1(){
 
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
-    @Before
-    public void setUp()
-    {
+    phoneBook.addToPhoneBook("Zebra", "111-222-333");;
+    
+    String expectedString = "[Zebra=[111-222-333]]";
+    String actualString = phoneBook.displayFromPhoneBook();
+    
+    assertEquals(expectedString, actualString);
     }
+    
+    @Test
+    public void testAddToPhoneBookIndividualNumbers(){
 
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @After
-    public void tearDown()
-    {
+    phoneBook.addToPhoneBookIndividualNumbers("Zebra", "111-222-333");
+    phoneBook.addToPhoneBookIndividualNumbers("Zebra", "222-444-4444");
+    
+    String expectedString = "[Zebra=[111-222-333, 222-444-4444]]";
+    String actualString = phoneBook.displayFromPhoneBook();
+    
+    assertEquals(expectedString, actualString);
+    
+    
+    }
+    
+    @Test
+    public void testRemoveFromPhoneBook1(){
+            PhoneBook phoneBook3 =new PhoneBook();
+    phoneBook3.addToPhoneBook("Zebra", "111-222-3333");
+    phoneBook3.addToPhoneBook("Dog", "222-444-4444");
+    phoneBook3.addToPhoneBook("Cat", "555-666-7777");
+    phoneBook3.addToPhoneBook("Alligator", "888-999-1010");
+    phoneBook3.addToPhoneBook("Wolf", "111-121-1313");
+    phoneBook3.removeFromPhoneBook("Dog");
+
+    String expectedString = "[Alligator=[888-999-1010], Cat=[555-666-7777], Wolf=[111-121-1313], Zebra=[111-222-3333]]";
+    
+    String actualString = phoneBook3.displayFromPhoneBook();
+    
+    assertEquals(expectedString, actualString);
+    
+    
+    }
+    
+    @Test
+    public void  testRemoveFromPhoneBookIndividualNumbers(){
+    phoneBook.addToPhoneBook("Zebra", "111-222-3333");
+    phoneBook.addToPhoneBookIndividualNumbers("Zebra", "222-444-4444");
+    phoneBook.removeFromPhoneBookIndividualNumbers("Zebra", "111-222-3333");
+    
+    String expectedString = "[Zebra=[222-444-4444]]";
+    String actualString = phoneBook.displayFromPhoneBook();
+    assertEquals(expectedString, actualString);
+    }
+    
+    @Test
+    public void testLookupFromPhoneBook(){
+                    PhoneBook phoneBook4 =new PhoneBook();
+    phoneBook4.addToPhoneBook("Zebra", "111-222-3333");
+    phoneBook4.addToPhoneBook("Dog", "222-444-4444");
+    phoneBook4.addToPhoneBook("Cat", "555-666-7777");
+    phoneBook4.addToPhoneBook("Alligator", "888-999-1010");
+    phoneBook4.addToPhoneBook("Wolf", "111-121-1313");
+    
+    String expectedString = "[555-666-7777]";
+    
+    
+    String actualString = phoneBook4.lookupFromPhoneBook("Cat");
+    
+    assertEquals(expectedString, actualString);
+    
+    
+    }
+    
+    @Test
+    public void testReverseLookup(){
+    phoneBook.addToPhoneBook("Zebra", "111-222-3333");
+    phoneBook.addToPhoneBook("Dog", "222-444-4444");
+    phoneBook.addToPhoneBook("Cat", "555-666-7777");
+    phoneBook.addToPhoneBook("Alligator", "888-999-1010");
+    phoneBook.addToPhoneBook("Wolf", "111-121-1313");
+    String expectedString = "Wolf";
+    
+    
+    String actualString = phoneBook.reverseLookupFromPhoneBook("111-121-1313");
+    
+    assertEquals(expectedString, actualString);
+    
+    
     }
 }
